253b1179731bd721298f28221e27ffcd9c432da4
pelleher03 <pelle.hermansen@gmail.com>
2022-01-18 09:56:12 +0100

    la til to tomme tiler, boot.py og main.py, små vikset litt på sensor koden min

--- vorstasjon_sensoravlesning.py
+++ vorstasjon_sensoravlesning.py
@@ -1,6 +1,6 @@
 from machine import Pin, I2C
-import dht
 import time
+import dht
 import CCS811
 from bmp280 import *
 
@@ -8,15 +8,14 @@ DHT = dht.DHT11(Pin(13))
 
 def main():
     i2c = I2C(scl=Pin(22), sda=Pin(21))
-    # Adafruit sensor breakout has i2c addr: 90; Sparkfun: 91
-    s = CCS811.CCS811(i2c=i2c, addr=90)
+    sensor = CCS811.CCS811(i2c=i2c, addr=90)
     bmp = BMP280(i2c, use_case=BMP280_CASE_WEATHER)
     time.sleep(1)
     
     while True:
-        if s.data_ready():
+        if sensor.data_ready():
             DHT.measure()
-            print('eCO2: %d ppm, TVOC: %d ppb' % (s.eCO2, s.tVOC))
+            print('eCO2: %d ppm, TVOC: %d ppb' % (sensor.eCO2, sensor.tVOC))
             print("temperatur:", DHT.temperature(), "luftfuktighet:", DHT.humidity())
             print("temp: ", bmp.temperature)
             print("pressure: ", bmp.pressure)
