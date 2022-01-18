544615f9d83f0052d7e169ee7b4167bc39ec2c0f
pelleher03 <pelle.hermansen@gmail.com>
2022-01-18 09:51:23 +0100

    Lagde et program som leser ut tempratur, VOC, luftfuktighet, og trykk

---
+++ vorstasjon_sensoravlesning.py
@@ -0,0 +1,25 @@
+from machine import Pin, I2C
+import dht
+import time
+import CCS811
+from bmp280 import *
+
+DHT = dht.DHT11(Pin(13))
+
+def main():
+    i2c = I2C(scl=Pin(22), sda=Pin(21))
+    # Adafruit sensor breakout has i2c addr: 90; Sparkfun: 91
+    s = CCS811.CCS811(i2c=i2c, addr=90)
+    bmp = BMP280(i2c, use_case=BMP280_CASE_WEATHER)
+    time.sleep(1)
+    
+    while True:
+        if s.data_ready():
+            DHT.measure()
+            print('eCO2: %d ppm, TVOC: %d ppb' % (s.eCO2, s.tVOC))
+            print("temperatur:", DHT.temperature(), "luftfuktighet:", DHT.humidity())
+            print("temp: ", bmp.temperature)
+            print("pressure: ", bmp.pressure)
+            print("")
+            time.sleep(1)
+main()
