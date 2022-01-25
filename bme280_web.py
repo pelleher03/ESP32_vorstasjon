e620e8f06597bac22278141deabfe2c79ee22e72
pelleher03 <pelle.hermansen@gmail.com>
2022-01-25 10:35:08 +0100

    Endret på css koden

--- bme280_web.py
+++ bme280_web.py
@@ -50,27 +50,30 @@ def web_page():
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="refresh" content="5">
-        <link rel="icon" href="data:,"><style>body { text-align: center; font-family: "Trebuchet MS", Arial;}
-          table { border-collapse: collapse; width:35%; margin-left:auto; margin-right:auto; }
-          th { padding: 12px; background-color: #0043af; color: white; }
-          tr { border: 1px solid #ddd; padding: 12px; }
-          tr:hover { background-color: #bcbcbc; }
-          td { border: none; padding: 12px; }
-          .sensor { color:white; font-weight: bold; background-color: #bcbcbc; padding: 1px;
-  </style>
-  </head>
-  <body>
-  <h1>ESP with BME280</h1>
-        <table>
-          <tr><th>MEASUREMENT</th><th>VALUE</th></tr>
-          <tr><td>Temp. Celsius(DHT11)</td><td><span class="sensor">""" + str(DHT.temperature()) + "C" +"""</span></td></tr>
-          <tr><td>Temp. Celsius</td><td><span class="sensor">""" + str(bme.temperature) + """</span></td></tr>
-          <tr><td>Pressure</td><td><span class="sensor">""" + str(bme.pressure) + """</span></td></tr>
-          <tr><td>Humidity</td><td><span class="sensor">""" + str(DHT.humidity()) + "%" + """</span></td></tr>
-          <tr><td>ECo2</td><td><span class="sensor">""" + str(sensor.eCO2) + """</span></td></tr>
-          <tr><td>TVOC</td><td><span class="sensor">""" + str(sensor.tVOC) + """</span></td></tr>
-        </table>
-    </body>
+        <link rel="icon" href="data:,">
+        <style>
+            body { text-align: center; background-color: rgb(69, 75, 79); font-family: "Trebuchet MS", Arial;}
+            H1 {color: rgb(227, 245, 64)}
+            table { border-collapse: collapse; width:35%; margin-left:auto; margin-right:auto; }
+            th { padding: 12px; background-color: #0043af; color: white; }
+            tr { border: 1px solid #ddd; padding: 12px; }
+            tr:hover { background-color: #9c9a9a;}
+            td { border: none; padding: 12px; color: white; background-color: #bcbcbc;}
+            .sensor { color:white; font-weight: bold; background-color: #bcbcbc; padding: 1px;}
+        </style>
+      </head>
+      <body>
+          <h1>ESP with BME280</h1>
+            <table>
+              <tr><th>MEASUREMENT</th><th>VALUE</th></tr>
+              <tr><td>Temp. Celsius(DHT11)</td><td><span class="sensor">""" + str(DHT.temperature()) + "C" +"""</span></td></tr>
+              <tr><td>Temp. Celsius</td><td><span class="sensor">""" + str(bme.temperature) + """</span></td></tr>
+              <tr><td>Pressure</td><td><span class="sensor">""" + str(bme.pressure) + """</span></td></tr>
+              <tr><td>Humidity</td><td><span class="sensor">""" + str(DHT.humidity()) + "%" + """</span></td></tr>
+              <tr><td>ECo2</td><td><span class="sensor">""" + str(sensor.eCO2) + """</span></td></tr>
+              <tr><td>TVOC</td><td><span class="sensor">""" + str(sensor.tVOC) + """</span></td></tr>
+            </table>
+      </body>
 </html>"""
       return html
 
